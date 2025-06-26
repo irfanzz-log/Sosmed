07ded53bb2537dd1cf0416007b3314800041d533
Irfan <Syah90009@gmail.com>
2025-06-26 22:13:57 +0700

    Test mini script v3

--- api/index.js
+++ api/index.js
@@ -1,9 +1,16 @@
 import express from "express";
 import serverless from "serverless-http";
+import path from "path"; // ✅ penting
+import { fileURLToPath } from "url";
 
 const app = express();
-app.set("views", path.join(__dirname, "views")); // TANPA ../
-app.use(express.static(path.join(__dirname, "public")));
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// setup EJS
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
 
 
 app.get("/", (req, res) => {
