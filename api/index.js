630fbacbc3d8578ab61352c5f09c7fedc49e14a5
Irfan <Syah90009@gmail.com>
2025-06-26 21:24:45 +0700

    fix route

--- api/index.js
+++ api/index.js
@@ -15,8 +15,8 @@ app.set('views', 'views');
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.set('views', path.join(__dirname, 'views'));
-app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, '../views'));
+app.use(express.static(path.join(__dirname, '../public')));
 
 (async () => {
   try {
